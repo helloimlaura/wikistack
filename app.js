17dd99291518c0b10246aef2b6cb0e4366e2cbe0
Javier Carey <javier3@gmail.com>
2018-08-03 09:50:44 -0700

    We have gotten the database hooked up with Sequelize and declared the Page and User objects and synced them with the database. We also dealt with the race condition of the website coming online before the db was connected.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 // const routes = require("./routes/posts");
+const { db, Page, User } = require("./models");
 
 const app = express();
 const layout = require("./views/layout");
@@ -17,11 +18,21 @@ app.use(express.urlencoded({
 
 app.get('/', (req, res) => {
   res.send(layout(''));
-})
-
-
-const PORT = 1980;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
 });
+
+// db.authenticate(). // this was just to check
+// then(() => {
+//   console.log('connected to the database');
+// });
+
+const syncAndConnect = async () => {
+  await Page.sync();
+  await User.sync();
+  console.log("Database is synced");
+  const PORT = 1980;
+  await app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+  });
+};
+
+syncAndConnect();
---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
+
+const Page = db.define('pages', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content:  {
+    type: Sequelize.TEXT
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  }
+});
+
+const User = db.define('users', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = { db, Page, User};
